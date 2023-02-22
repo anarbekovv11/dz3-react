2991bbcde2966074e3dbe0ea3b056905ff44c0ee
anarbekovv11 <anarbekovvv11@gamil.com>
2023-02-22 11:38:34 +0600

    homework3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            age: ""
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.setState({name: "Geeks", age: "15"});
+    }
+    
+    render() {
+        return (
+            <div className="block">
+                <p className="p">Имя: {this.state.name}, Возраст : {this.state.age}</p>
+                <button onClick={this.handleClick} className="btn">Press</button>
+            </div> 
+        )
+    }
+    
 }
 
-export default App;
+
+export default Button;
